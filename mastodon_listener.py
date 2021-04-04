24eefc9e733ae323d5c59474a00c78cfca3aead6
Shura0 <shura0@yandex.ru>
2021-04-04 22:06:27 +0300

    mentions in reblog messages are fixed

--- mastodon_listener.py
+++ mastodon_listener.py
@@ -95,6 +95,9 @@ class MastodonListener(StreamListener):
                 acct=acct+'@'+self.server_name
             m.from_mid=acct
             m.add_mentions("@" + acct)
+            if not '@' in data['reblog']['account']['acct']:
+                acct=data['reblog']['account']['acct']+'@'+self.server_name
+            m.add_mentions("@" + acct)
             to_out = "@{} reblog status of @{}:\n".format(
                 data['account']['acct'],
                 data['reblog']['account']['acct'])
